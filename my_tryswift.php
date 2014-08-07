8b70bf39923d33c2b2fc0967c04370e2a64ca4a3
Yangxinye Yang <sophiayang@bridled.ci.uchicago.edu>
2014-08-07 13:18:48 -0500

    add original

--- my_tryswift.php
+++ my_tryswift.php
@@ -5,21 +5,22 @@
       }   
    }
 
+   // var_dump($_POST["source"]);
    $source = $_POST['source'];
    $web_directory = "/home/sophiayang/public_html/try-swift";
-   $swift_cmd = "nohup /home/sophiayang/public_html/try-swift/swift-0.95-RC6/bin/swift -sites.file sites.xml -tc.file tc.data -config cf script.swift";
+   $swift_cmd = "/home/sophiayang/public_html/try-swift/swift-0.95-RC6/bin/swift script.swift -site=local";
 
    # Create directory structure
    $unique = uniqid();
    $dirname = $web_directory . "/runs/" . $unique;
    umask(0);
+
+
    if (!mkdir($dirname, 0755)) { die("Failed to create folder $dirname"); }
    if (!chdir($dirname))       { die("Unable to chdir to $dirname"); }
-
+   system("whoami > whoami.txt");
    # Copy and create Swift files
-   copy_file("$web_directory/config/sites.xml", "$dirname/sites.xml");
-   copy_file("$web_directory/config/tc.data", "$dirname/tc.data");
-   copy_file("$web_directory/config/cf", "$dirname/cf");
+   copy_file("$web_directory/config/swift.properties", "$dirname/swift.properties");
    $script = $dirname . "/script.swift";
 
    if(!file_put_contents($script, $source)) {
@@ -27,10 +28,10 @@
    }
 
    # Run Swift
-#system("export PATH=/usr/local/jdk1.6.0/bin:$PATH");
    system("echo Swift run starting at $( date +%I:%M:%S ) > $dirname/swift.out");
-   system("$swift_cmd 2>&1 | sed -u -e 's/^[ \t]*//' -e s/'Selecting site:'/Ready:/g -e s/'Finished successfully:'/Done:/g >> $dirname/swift.out &");
-
+   # system("which java >> $dirname/swift.out ");
+   # system("$swift_cmd -version >> $dirname/swift.out");
+   system("$swift_cmd > $dirname/swift.out 2>&1 ");
    print "runs/$unique/swift.out\n";
    print "runs/$unique\n";
 ?>
