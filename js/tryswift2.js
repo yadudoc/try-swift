6622faac4136d2ae3a6a866eaaf0ad8b6728e300
Yangxinye Yang <yangyangxinye@gmail.com>
2014-08-13 15:09:35 -0500

    more

--- js/tryswift2.js
+++ js/tryswift2.js
@@ -19,6 +19,51 @@ function setVisiblePage(n) {
   visiblePage.classList.remove('hidden');
 }
 
+// function page_convert(n) {
+// 	return n<10? "0" + n: n;
+// }
+
+function hideFiles() {
+    document.getElementById('outputs').innerHTML = "<option>File outputs</option>";
+    $('#outputs').hide();
+}
+
+function check_buttons() {
+	$('#topics').val(index[currentPage-1]);
+	if (currentPage == maxPage) {
+	    next.setAttribute('disabled', true);
+	    prev.removeAttribute('disabled');
+	} else if (currentPage == 1) {
+	    prev.setAttribute('disabled', true);
+	    next.removeAttribute('disabled');
+	} else {
+		prev.removeAttribute('disabled');
+		next.removeAttribute('disabled');
+	}
+}
+
+function show_next() {
+	currentPage++;
+	if (currentPage <= maxPage) {
+		setVisiblePage(currentPage);
+		editor.setValue($('#source-' + currentPage).text(), -1);
+		document.getElementById('swiftOutput').innerHTML = "";
+		hideFiles();
+		check_buttons();
+	}
+}
+
+function show_prev() {
+	currentPage--;
+	if (currentPage > 0) {
+		setVisiblePage(currentPage);
+		editor.setValue($('#source-' + currentPage).text(), -1);
+		document.getElementById('swiftOutput').innerHTML = "";
+		hideFiles();
+		check_buttons();
+	} 
+}
+
 $(document).ready(function () {
 	$.get('scripts/index.txt', function(data) {	
 		index = data.split("\n");
@@ -40,12 +85,40 @@ $(document).ready(function () {
 			source.className = 'sourceToHide';
 			document.getElementById('page-'+i).appendChild(source);
 		}
+
+		next = document.getElementById('nextButton');
+		prev = document.getElementById('previousButton');
+		reset = document.getElementById('resetButton');
+		execute = document.getElementById('executeButton');
+		topics = document.getElementById('topics');
+
+		next.addEventListener('click', show_next);
+		prev.addEventListener('click', show_prev);
+		// reset.addEventListener('click', reset_text);
+		// execute.addEventListener('click', execute_code);
 		
 		setVisiblePage(1);
-		editor.setValue($('#source-' + 01).text(), -1);
+		editor.setValue($('#source-' + 4).text(), -1);
 		
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
